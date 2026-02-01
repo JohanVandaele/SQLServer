95f3f0510ac058021263c0336c1b2613505725c8
Johan Vandaele <johanvandaele@hotmail.com>
2026-02-01 11:33:16 +0100

    Add files via upload

---
+++ Solution/LocalDBSolution/LocalDB/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Data;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+try
+{
+    // --------------------
+    // Get ConnectionString
+    // --------------------
+    var configuration = new ConfigurationBuilder()
+        .SetBasePath(Directory.GetCurrentDirectory())      // Optional
+        .AddJsonFile("appsettings.json", false, true)
+        .Build();
+
+    //var connectionString = configuration.GetConnectionString("KlantenDB");    // or
+    var connectionString = configuration["ConnectionStrings:KlantenDB"];        // or
+
+    //var connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|Database.mdf;TrustServerCertificate=True";
+
+    // ----------
+    // Connection
+    // ----------
+    using var connection = new SqlConnection(connectionString);
+
+    // -------
+    // Command
+    // -------
+    var comKlanten = connection.CreateCommand();
+    comKlanten.CommandType = CommandType.Text;
+    comKlanten.CommandText = "select Voornaam, Familienaam from klanten";
+
+    connection.Open();
+    Console.WriteLine("Database geöpend");
+
+    // ------
+    // Reader
+    // ------
+    using var rdrKlanten = comKlanten.ExecuteReader();
+
+    var klantFamilienaamPos = rdrKlanten.GetOrdinal("Familienaam");
+    var klantVoornaamPos = rdrKlanten.GetOrdinal("Voornaam");
+
+    while (rdrKlanten.Read())
+        Console.WriteLine($"{rdrKlanten.GetString(klantVoornaamPos)} {rdrKlanten.GetString(klantFamilienaamPos)}");
+
+    Console.WriteLine();
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"{ex.Message}");
+}
